62162888aca075b8807d19def02e454c3529fd21
srnb1 <hanisky1@naver.com>
2024-07-13 15:10:59 +0900

    gradle 수준+ manifest에 firebase, kakao api implementation, 애플의 경우 기기가 없어 어려움을 확인. StartLoginActivity로 mainActivity를 바꾸고 세 번째 버튼을 누를 시 나중의 메인 화면으로 전환되도록 코드 수정.

--- app/src/main/java/com/planetcopapplication/forever/StartLoginActivity.kt
+++ app/src/main/java/com/planetcopapplication/forever/StartLoginActivity.kt
@@ -1,6 +1,8 @@
 package com.planetcopapplication.forever
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,11 @@ class StartLoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_start_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        var mainActivityBtn = findViewById<Button>(R.id.appleLoginBtn)
+        mainActivityBtn.setOnClickListener{
+            var intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
