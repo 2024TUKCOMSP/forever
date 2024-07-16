c9cae4d3be1104528d3254381ae762e6d604e1e8
jzongyun <jeong.yun@tukorea.ac.kr>
2024-07-16 15:31:29 +0900

    feat : make base statement

---
+++ forever_planner/src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import CalendarView from '../views/CalendarView.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'calendar',
+      component: CalendarView,
+    },
+  ],
+});
+
+export default router;
---
+++ forever_planner/src/views/CalendarView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1 class="bg-black text-white">Calendar View</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CalendarView',
+};
+</script>
+
+<style scoped>
+</style>
