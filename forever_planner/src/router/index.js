1a9ee9b474dc53d5c96e4ad047d5b00f96b0fd69
jzongyun <jeong.yun@tukorea.ac.kr>
2024-07-16 15:40:22 +0900

    feat : make home view page

--- forever_planner/src/router/index.js
+++ forever_planner/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import CalendarView from '../views/CalendarView.vue';
+import HomeView from '../views/HomeView.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -9,6 +10,11 @@ const router = createRouter({
       path: '/',
       name: 'calendar',
       component: CalendarView,
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: HomeView,
     },
   ],
 });
--- forever_planner/src/views/HomeView.vue
+++ forever_planner/src/views/HomeView.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <h1 class="bg-black text-white">Calendar View</h1>
+    <h1>Home View</h1>
   </div>
 </template>
-
+  
 <script>
 </script>
-
+  
 <style scoped>
 </style>
