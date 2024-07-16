6c7ce051d47683af4ca7834a8816e5bc20801c75
jzongyun <jeong.yun@tukorea.ac.kr>
2024-07-17 03:29:45 +0900

    feat : make post component

---
+++ forever_planner/src/components/Calendar/PostVue.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="p-0.5 w-full">
+    <div class="bg-[#e4eefc] w-full rounded text-2xs p-[0.5px] flex items-center justify-center">방학</div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
