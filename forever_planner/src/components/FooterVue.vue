10e5c47d9e47a776cb004f466e82f12b1b2eec5d
jzongyun <jeong.yun@tukorea.ac.kr>
2024-07-18 16:46:39 +0900

    fix : fix error

--- forever_planner/src/components/FooterVue.vue
+++ forever_planner/src/components/FooterVue.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="w-full items-center h-12 bg-[#f5f7fd] flex justify-around">
-    <div :class="isClicked == 'home' ? 'clicked-icon-color' : 'icon-color'" @click="clickHomeButton()">
+    <div :class="isClicked === 'home' ? 'clicked-icon-color' : 'icon-color'" @click="clickHomeButton">
       <i class="w-6 h-6 fa-solid fa-house"></i>
     </div>
-    <div :class="isClicked == 'diary' ? 'clicked-icon-color' : 'icon-color'" @click="clickDiaryButton()">
+    <div :class="isClicked === 'diary' ? 'clicked-icon-color' : 'icon-color'" @click="clickDiaryButton">
       <i class="w-6 h-6 fa-solid fa-calendar-day"></i>
     </div>
-    <div :class="isClicked == 'theme' ? 'clicked-icon-color' : 'icon-color'" @click="clickThemeButton()">
+    <div :class="isClicked === 'theme' ? 'clicked-icon-color' : 'icon-color'" @click="clickThemeButton">
       <i class="w-6 h-6 fa-solid fa-palette"></i>
     </div>
   </div>
@@ -15,15 +15,22 @@
 <script setup>
 import { storeToRefs } from 'pinia';
 import { useStore } from '@/stores/store.js';
+import { useRouter } from 'vue-router';
 
-const { isClicked } = storeToRefs(useStore());
+const store = useStore();
+const { isClicked } = storeToRefs(store);
+const router = useRouter();
 
 const clickHomeButton = () => {
   isClicked.value = 'home';
+  router.push({ name: 'home' });
 };
+
 const clickDiaryButton = () => {
   isClicked.value = 'diary';
+  router.push({ name: 'calendar' });
 };
+
 const clickThemeButton = () => {
   isClicked.value = 'theme';
 };
--- forever_planner/src/views/HomeView.vue
+++ forever_planner/src/views/HomeView.vue
@@ -1,24 +1,26 @@
 <template>
   <div class="h-screen flex flex-col">
     <div class="planetTxtBar">
-      <button type="button" @click="planetBtnClick()" class="planetTxtBtn">Planet v</button>
-      <button type="button" @click="goSettingBtnClick()" class="goSettingBtn">설정</button>
+      <button type="button" @click="planetBtnClick" class="planetTxtBtn">Planet v</button>
+      <button type="button" @click="goSettingBtnClick" class="goSettingBtn">설정</button>
     </div>
-    <div class="flex-grow w-full bg-[#f1f5f8] p-2 flex flex-col">
-      <div class="userBar">
-        <div class="userIcon">
-          <button type="button" @click="userIconClick()" class="userIconBtn">클릭</button>
-          <p class="userName">디폴트이름</p>
+    <div class="flex-grow w-full bg-[#f5f7fd] p-2">
+      <div class="w-full flex flex-col">
+        <div class="userBar">
+          <div class="userIcon">
+            <button type="button" @click="userIconClick" class="userIconBtn">클릭</button>
+            <p class="userName">디폴트이름</p>
+          </div>
         </div>
-      </div>
-      <button type="button" class="remainingTodoBtn" @click="remainingTodoClick()">
-        <span>2개의 남은 할 일</span>
-        <span class="remainingTodoTxt">&gt;</span>
-      </button>
-      <div class="todaysTodo">
-        <p>오늘</p>
-        <p class="todaysTodoDate">0월 0일</p>
-        <button type="button" class="todoEditBtn" @click="todaysTodoDateClick()">+ 할 일을 추가하세요</button>
+        <button type="button" class="remainingTodoBtn" @click="remainingTodoClick">
+          <span>2개의 남은 할 일</span>
+          <span class="remainingTodoTxt">&gt;</span>
+        </button>
+        <div class="todaysTodo">
+          <p>오늘</p>
+          <p class="todaysTodoDate">0월 0일</p>
+          <button type="button" class="todoEditBtn" @click="todaysTodoDateClick">+ 할 일을 추가하세요</button>
+        </div>
       </div>
     </div>
     <FooterVue />
@@ -26,19 +28,18 @@
 </template>
 
 <script>
-import { useRouter } from 'vue-router';
-import FooterVue from '@/components/FooterVue.vue'; // FooterVue 컴포넌트 등록
+import FooterVue from '@/components/FooterVue.vue';
 
 export default {
-  name: 'HomeView',
+  name: 'Home-View',
   components: {
     FooterVue,
   },
-  setup() {
-    const router = useRouter();
-    return {
-      router,
-    };
+  data() {
+    return {};
+  },
+  mounted() {
+    this.isClicked = 'home';
   },
   methods: {
     userIconClick() {
@@ -51,20 +52,20 @@ export default {
     planetBtnClick() {
 
     },
+    goSettingBtnClick() {
+      this.$router.push({ name: 'setting' });
+    },
     todaysTodoDateClick() {
 
     },
-    goSettingBtnClick() {
-      this.router.push({ path: '/setting' });
-    },
   },
-}
+};
 </script>
 
 <style scoped>
 .planetTxtBar {
   padding-left: 10px;
-  background-color: #F1F5F8;
+  background-color: #f5f7fd;
   position: relative;
   font-size: larger;
   color: #8F9095;
@@ -78,7 +79,7 @@ export default {
 
 }
 .background {
-  background-color: #F1F5F8;
+  background-color: #f5f7fd;
 }
 .goSettingBtn {
   font-size: medium;
