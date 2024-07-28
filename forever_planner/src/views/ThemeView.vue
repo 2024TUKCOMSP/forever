bd95d9ea1d4c271bd195d49fc080bcd3641c63e9
jzongyun <jeong.yun@tukorea.ac.kr>
2024-07-29 05:18:46 +0900

    feat : link api to display theme on theme page

--- forever_planner/src/views/ThemeView.vue
+++ forever_planner/src/views/ThemeView.vue
@@ -3,8 +3,9 @@
     <div class="w-full h-full bg-[#f5f7fd] p-4 flex flex-col">
       <div class="text-xl font-semibold py-4">Colors</div>
       <div class="flex gap-4 flex-col">
-        <div v-for="theme in isSelected" :key="theme">
-          <ThemeVue :isSelected="theme" />
+        <div v-for="theme in colors" :key="theme">
+          <ThemeVue 
+            :theme="theme" />
         </div>
       </div>
     </div>
@@ -13,11 +14,23 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
+import axios from 'axios';
 import FooterVue from '@/components/FooterVue.vue';
 import ThemeVue from '@/components/Theme/ThemeVue.vue';
 
 const isSelected = ref([false, true]);
+const colors = ref([]);
+
+const getColors = async () => {
+  const res = await axios.get(`http://34.146.205.159:8000/theme/all/?format=json`);
+  colors.value = res.data;
+};
+
+onMounted(async() => {
+  await getColors();
+  console.log(colors.value);
+});
 </script>
 
 <style scoped>
